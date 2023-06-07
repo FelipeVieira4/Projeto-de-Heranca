9e65f710dcd61f7239ecfe3b2517f36018ed0ca6
Felipe Vieira <101891565+FelipeVieira4@users.noreply.github.com>
2023-06-07 20:59:20 -0300

    Update PrincipalScreen.java

--- src/view/PrincipalScreen.java
+++ src/view/PrincipalScreen.java
@@ -5,6 +5,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTabbedPane;
+import javax.swing.JLabel;
 
 public class PrincipalScreen extends JFrame {
 
@@ -35,11 +37,27 @@ public class PrincipalScreen extends JFrame {
 	 */
 	public PrincipalScreen() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 642, 424);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(10, 11, 606, 363);
+		contentPane.add(tabbedPane);
+		
+		JPanel cadastroPane = new JPanel();
+		tabbedPane.addTab("Cadastro", null, cadastroPane, null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		cadastroPane.add(lblNewLabel);
+		
+		JPanel buscarPane = new JPanel();
+		tabbedPane.addTab("Buscar", null, buscarPane, null);
+		
+		JPanel panel_2 = new JPanel();
+		tabbedPane.addTab("New tab", null, panel_2, null);
 	}
-
 }
